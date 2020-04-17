64861bf2becb395373354cf9eb094e1ef5b62b69
Shubham Verma <var.subham@gmail.com>
2020-04-17 19:14:34 +0530

    updated

--- app/src/main/java/com/example/earthquake/QuakeReport.java
+++ app/src/main/java/com/example/earthquake/QuakeReport.java
@@ -17,7 +17,8 @@ import java.util.ArrayList;
 
 public class QuakeReport extends AppCompatActivity {
     ArrayList<EarthquakeClass> earthquakes = new ArrayList<>();
-    final String  url_website_usgs = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    final String  url_website_usgs = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=4&limit=20";
+    CustomAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +30,18 @@ public class QuakeReport extends AppCompatActivity {
 
         final ListView listView = findViewById(R.id.list_view);
 
-//        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(QuakeReport.this, android.R.layout.simple_list_item_1, earthquakes);
-//        CustomAdapter customAdapter = new CustomAdapter(QuakeReport.this, earthquakes);
-//        listView.setAdapter(customAdapter);
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                EarthquakeClass obj = earthquakes.get(position);
-//                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(obj.getUrl()));
-//                startActivity(intent);
-//            }
-//        });
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(QuakeReport.this, android.R.layout.simple_list_item_1, earthquakes);
+        customAdapter = new CustomAdapter(QuakeReport.this, new ArrayList<EarthquakeClass>());
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EarthquakeClass obj = earthquakes.get(position);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(obj.getUrl()));
+                startActivity(intent);
+            }
+        });
 
 
 
@@ -49,10 +50,13 @@ public class QuakeReport extends AppCompatActivity {
     private class myasyncclass extends AsyncTask<String, Void, ArrayList<EarthquakeClass>>{
         @Override
         protected void onPostExecute(ArrayList<EarthquakeClass> earthquakeClasses) {
-            final ListView listView = findViewById(R.id.list_view);
+            //final ListView listView = findViewById(R.id.list_view);
             //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(QuakeReport.this, android.R.layout.simple_list_item_1, earthquakes);
-            CustomAdapter customAdapter = new CustomAdapter(QuakeReport.this, earthquakeClasses);
-            listView.setAdapter(customAdapter);
+            //customAdapter = new CustomAdapter(QuakeReport.this, earthquakeClasses);
+
+            customAdapter.clear();
+            if(earthquakeClasses != null && !earthquakeClasses.isEmpty());
+                customAdapter.addAll(earthquakeClasses);
         }
 
         @Override
